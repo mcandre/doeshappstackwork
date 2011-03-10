325a4e6d03e6c589cb1b9a693d8c5ca8a1c8ff7b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-03-10 10:42:23 -0500

    Added README and daemonizer.

---
+++ happstack.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# Install:
+#
+# chmod a+x happstack.sh
+# sudo cp happstack.sh /etc/init.d/happstack
+# sudo update-rc.d happstack defaults
+#
+# Uninstall:
+#
+# sudo rm /etc/rc*/happstack
+# sudo rm /etc/init.d/happstack
+
+HAPPSTACK_DIR=/srv/d_hactar/usr/src/doeshappstackwork
+
+case "$1" in
+  start)
+    echo "Starting Happstack..."
+    $HAPPSTACK_DIR/doeshappstackwork.hs > /dev/null 2>&1 &
+    ;;
+  stop)
+    echo "Stopping Happstack..."
+    killall doeshappstackwork.hs
+    ;;
+  restart)
+    echo "Restarting Happstack..."
+    killall doeshappstackwork.hs
+    $HAPPSTACK_DIR/doeshappstackwork.hs > /dev/null 2>&1 &
+    ;;
+  *)
+    echo "Usage: $0 start|stop|restart"
+    exit 1
+    ;;
+esac
+
+exit 0
